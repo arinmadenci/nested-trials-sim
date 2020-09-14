3fbf79bbc5dae41621a5d7ec49e182aea1e5eb90
arin <arin.madenci@gmail.com>
2020-09-14 11:29:58 -0400

    changed model formula names to avoid conflict between ipw and standardization

--- plr-ipw-script.R
+++ plr-ipw-script.R
@@ -4,14 +4,15 @@ if (!require("pacman")) install.packages("pacman"); library(pacman); p_load(here
 source("plr-ipw-function.R")
 load(here::here("expanded-dat.Rda"))
 
-formula.treatment <- "group.binary ~ 
+ipw_formulas <- list()
+ipw_formulas$formula.treatment <- "group.binary ~ 
                                 gender + 
                                 ns(systolic_bl, knots=quantile(systolic_bl, probs=c(0.35, 0.65)), Boundary.knots=quantile(systolic_bl, probs=c(0.1, 0.9))) + 
                                 ns(t_bl, knots=quantile(t_bl, probs=c(0.35, 0.65)), Boundary.knots=quantile(t_bl, probs=c(0.1, 0.9))) +
                                 ns(calendar.time_bl, knots=quantile(calendar.time_bl, probs=c(0.35, 0.65)), Boundary.knots=quantile(calendar.time_bl, probs=c(0.1, 0.9)))" # sample treatment regression
 
-formula.outcome <- "mortality ~ 
+ipw_formulas$formula.outcome <- "mortality ~ 
                                 group.binary*ns(t.new, knots=quantile(t.new, probs=c(0.35, 0.65)), Boundary.knots=quantile(t.new, probs=c(0.1, 0.9)))" # sample outcome regression
 
 plr_ipw_function(dat=expanded.dat, followupdays=30, 
-                 formula.treatment=formula.treatment, formula.treatment.numerator=NULL, formula.outcome=formula.outcome)
+                 formula.treatment=ipw_formulas$formula.treatment, formula.treatment.numerator=NULL, formula.outcome=ipw_formulas$formula.outcome)
--- plr-standardization-script.R
+++ plr-standardization-script.R
@@ -3,11 +3,12 @@ if (!require("pacman")) install.packages("pacman"); library(pacman); p_load(here
 
 source("plr-standardization-function.R")
 load(here::here("expanded-dat.Rda"))
-formula.outcome <- "mortality ~ 
+standardization_formulas <- list()
+standardization_formulas$formula.outcome <- "mortality ~ 
                                 group*ns(t.new, knots=quantile(t.new, probs=c(0.35, 0.65)), Boundary.knots=quantile(t.new, probs=c(0.1, 0.9))) + 
                                 gender + 
                                 ns(systolic_bl, knots=quantile(systolic_bl, probs=c(0.35, 0.65)), Boundary.knots=quantile(systolic_bl, probs=c(0.1, 0.9))) + 
                                 ns(t_bl, knots=quantile(t_bl, probs=c(0.35, 0.65)), Boundary.knots=quantile(t_bl, probs=c(0.1, 0.9)))
                                 ns(calendar.time_bl, knots=quantile(calendar.time_bl, probs=c(0.35, 0.65)), Boundary.knots=quantile(calendar.time_bl, probs=c(0.1, 0.9)))" # sample outcome regression
 
-plr_standardization_function(dat=expanded.dat, formula.outcome=formula.outcome, followupdays=30)
+plr_standardization_function(dat=expanded.dat, formula.outcome=standardization_formulas$formula.outcome, followupdays=30)
